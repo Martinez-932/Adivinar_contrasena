42742a625ead4da28562b12e93ce55b23d43e004
Martinez-932 <mm23042@ues.edu.sv>
2023-09-23 17:34:58 -0600

    Primer commit

---
+++ src/contraseña/Contraseña.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package contraseña;
+import java.util.Random;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Contraseña {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+        String caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+        int longitud = 10;
+        Random random = new Random();
+        StringBuilder contrasena = new StringBuilder();
+        for (int i = 0; i < longitud; i++) {
+            int indice = random.nextInt(caracteres.length());
+            char caracter = caracteres.charAt(indice);
+            contrasena.append(caracter);
+        }
+        System.out.println("Contraseña generada: " + contrasena.toString());
+    }
+}
+    
+    
+
